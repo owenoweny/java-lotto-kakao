b2e8a92710662e0f95c599905edf452762e1cca6
owenoweny <na_chi@naver.com>
2024-04-02 15:12:15 +0900

    feat: 콘솔 진행 controller, view 구현

---
+++ src/main/java/LottoController.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class LottoController {
+    private final LottoConsoleView lottoConsoleView;
+
+    public LottoController(LottoConsoleView lottoConsoleView) {
+        this.lottoConsoleView = lottoConsoleView;
+    }
+
+    public void run() {
+        int money = lottoConsoleView.getMoneyInput();
+        Lottos lottos = LottoMachine.issue(money);
+        lottoConsoleView.printIssuedLottos(lottos);
+
+        List<Integer> winningNumbersInput = lottoConsoleView.getWinningNumbersInput();
+        int bonusBallInput = lottoConsoleView.getBonusBallInput();
+
+        WinningLotto winningLotto = new WinningLotto(Lotto.from(winningNumbersInput), new LottoNumber(bonusBallInput));
+        LottoManager lottoManager = new LottoManager(lottos, winningLotto);
+
+        lottoConsoleView.printWinningResult(lottoManager.winningResults());
+        lottoConsoleView.printRevenue(lottoManager.revenueRate());
+    }
+
+    public static void main(String[] args) {
+        new LottoController(new LottoConsoleView(new Scanner(System.in))).run();
+    }
+}
